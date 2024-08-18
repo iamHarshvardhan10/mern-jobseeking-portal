ec052f8a5c4ff75391bbb67164e1549e6cf31b7e
Harshvardhan <shreeharshpb11@gmail.com>
2024-08-18 21:47:30 +0530

    create verifyUser middleware functionality

---
+++ backend/middlewares/verifyUser.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken'
+
+export const verifyUser = async (req, res, next) => {
+    try {
+        const token = req.cookies.token || req.body.token || req.header('Authorization')?.replace('Bearer ', '')
+        if (!token) {
+            return res.status(401).json({
+                message: 'Unauthorized',
+                success: false
+            })
+        }
+        try {
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            req.user = decoded
+        } catch (error) {
+            return res.status(401).json({
+                message: 'Invalid token',
+                success: false
+            })
+        }
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error verifying user",
+            error: error.message
+        })
+    }
+}
