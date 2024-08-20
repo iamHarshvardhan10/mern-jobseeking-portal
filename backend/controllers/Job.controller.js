922ce1b8df5fcf57ce407de60a417f75e7e0707b
Harshvardhan <shreeharshpb11@gmail.com>
2024-08-20 20:26:46 +0530

    create getAdminJob controller and route functionality

--- backend/controllers/Job.controller.js
+++ backend/controllers/Job.controller.js
@@ -82,3 +82,31 @@ export const getAllJobs = async (req, res) => {
         })
     }
 }
+
+// get Job by Id
+
+export const getJobById = async (req, res) => {
+    try {
+        const jobId = req.params.id;
+        const job = await Job.findById(jobId)
+            .populate({ path: 'company' })
+
+        if (!job) {
+            return res.status(404).json({
+                success: false,
+                message: 'No Job Found'
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Job retrieved successfully",
+            job
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Error fetching job",
+        })
+    }
+}
--- backend/routes/Job.route.js
+++ backend/routes/Job.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { verifyUser } from '../middlewares/verifyUser.js';
-import { getAllJobs, postJob } from '../controllers/Job.controller.js';
+import { getAllJobs, getJobById, postJob } from '../controllers/Job.controller.js';
 
 const router = express.Router()
 
@@ -9,4 +9,6 @@ const router = express.Router()
 router.post('/postJob', verifyUser, postJob)
 // getAll jobs
 router.get('/getAllJobs', verifyUser, getAllJobs)
+// get job by id
+router.get('/getAdminJob/:id', verifyUser, getJobById)
 export default router;
