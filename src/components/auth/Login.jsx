367d928b66ff381ae693dbe33e6253a050ef64cb
Harshvardhan <shreeharshpb11@gmail.com>
2024-08-23 11:50:30 +0530

    create Login Page UI

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,9 +1,67 @@
-
+import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { RadioGroup } from "../ui/radio-group";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <div className="flex items-center justify-center max-w-7xl mx-auto">
+        <form
+          action=""
+          className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
+        >
+          <h1 className="font-bold text-xl mb-5">Login</h1>
+          <div className="my-2">
+            <Label>Email</Label>
+            <Input type="text" placeholder="Example@gmail.com" />
+          </div>
+          <div className="my-2">
+            <Label>Password</Label>
+            <Input type="password" placeholder="password" />
+          </div>
+          <div className="my-2">
+            <Label>Roles</Label>
+            <div>
+              <RadioGroup
+                defaultValue="comfortable"
+                className="flex items-center gap-8 my-4"
+              >
+                <div className="flex items-center space-x-2">
+                  <Input
+                    type="radio"
+                    name="role"
+                    value="Student"
+                    className="cursor-pointer"
+                  />
+                  <Label htmlFor="r1">Student</Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <Input
+                    type="radio"
+                    name="role"
+                    value="Recruiter"
+                    className="cursor-pointer"
+                  />
+                  <Label htmlFor="r2">Recruiter</Label>
+                </div>
+              </RadioGroup>
+            </div>
+          </div>
+          <Button type="submit" className="w-full my-4">
+            Login
+          </Button>
+          <span className="text-sm">
+            Dont Have An Account?
+            <Link to={"/sign-up"} className="text-blue-600">
+              Sing Up
+            </Link>
+          </span>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
