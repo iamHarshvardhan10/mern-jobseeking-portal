5a0e01cc04170d3a8d86d6fbce00a29329d513fa
Harshvardhan <shreeharshpb11@gmail.com>
2024-08-25 20:18:18 +0530

    create Home LatestJobs page UI

---
+++ src/components/LatestJobsCard.jsx
@@ -0,0 +1,31 @@
+import { Badge } from "./ui/badge";
+
+const LatestJobsCard = () => {
+  return (
+    <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer">
+      <div>
+        <h1 className="font-medium text-lg">Company-1</h1>
+        <p className="text-sm text-gray-500">India</p>
+      </div>
+      <div>
+        <h1 className="font-bold text-lg my-2">Job1</h1>
+        <p className="text-sm text-gray-600">Desc1</p>
+      </div>
+      <div>
+        <div className="flex items-center gap-2 m-4">
+          <Badge className={"text-blue-700 font-bold"} variant="ghost">
+            12 Positions
+          </Badge>
+          <Badge className={"text-[#F83002] font-bold"} variant="ghost">
+            developer
+          </Badge>
+          <Badge className={"text-[#7209b7] font-bold"} variant="ghost">
+            12 LPA
+          </Badge>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LatestJobsCard;
--- src/components/shared/Home.jsx
+++ src/components/shared/Home.jsx
@@ -1,11 +1,13 @@
 import CategoryCarousel from "../CategoryCarousel";
 import HeroSection from "../HeroSection";
+import LatestJobs from "../LatestJobs";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <CategoryCarousel />
+      <LatestJobs/>
     </div>
   );
 };
