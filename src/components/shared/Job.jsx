07a5d1deef19955c8bbf4ad393e818d5fab54289
Harshvardhan <shreeharshpb11@gmail.com>
2024-08-26 13:18:16 +0530

    create Job Page UI

--- src/components/shared/Job.jsx
+++ src/components/shared/Job.jsx
@@ -1,9 +1,29 @@
+import FilterCard from "../FilterCard";
+import JobCard from "../JobCard";
 
+const fileterJobs = [1, 2, 3, 4, 5, 6, 7, 8];
 
 const Job = () => {
   return (
-    <div>Job</div>
-  )
-}
+    <div className="max-w-7xl mx-auto mt-5">
+      <div className="flex gap-5">
+        <div className="w-[20%]">
+          <FilterCard />
+        </div>
+        {fileterJobs.length <= 0 ? (
+          <span>Job Not Found</span>
+        ) : (
+          <div className="flex-1 h-[88vh] overflow-y-auto pb-5">
+            <div className="grid grid-cols-3 gap-4">
+              {fileterJobs.map((job, index) => (
+                <JobCard key={index} />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Job
+export default Job;
